dd2338b06c224a5c0d74cd533de673452744f6c8
Fatih Goncagül <fthgnc48@icloud.com>
2024-05-07 00:24:12 +0300

    Implemented JWT-based authentication for secure access to protected resources.

---
+++ src/main/java/com/example/springsecdemo/config/JwtFilter.java
@@ -0,0 +1,59 @@
+package com.example.springsecdemo.config;
+
+
+//we want this filter to be used in all requests
+
+import com.example.springsecdemo.service.JwtService;
+import com.example.springsecdemo.service.MyUserDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Autowired
+    JwtService jwtService;
+    @Autowired
+    ApplicationContext context;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+        String token = null;
+        String userName = null;
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+
+            token = authHeader.substring(7);
+            userName = jwtService.extractUserName(token);
+        }
+
+        if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = context.getBean(MyUserDetailsService.class).loadUserByUsername(userName);
+            if (jwtService.validateToken(token, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request)); // token also needs the request
+                //we got the authobject that spring needed (remember security config class, wwe set before usernamepaswordauth we set our filter)
+                //but we need to set it to security context
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+
+
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
--- src/main/java/com/example/springsecdemo/model/User.java
+++ src/main/java/com/example/springsecdemo/model/User.java
@@ -1,9 +1,7 @@
 package com.example.springsecdemo.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
@@ -12,6 +10,7 @@ import lombok.Data;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String username;
